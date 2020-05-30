235c11de0cf7f1b1ef119204f214a81cf07739c4
Documentation Builder <project@mandryn.org>
2020-05-30 16:35:32 +0000

    Automated documentation build for branch sampleiterator-doc, changeset 834c385ca490150e9f5f8ffa63f5679e17481353.

---
+++ sampleiterator-doc/navtreedata.js
@@ -0,0 +1,56 @@
+var NAVTREE =
+[
+  [ "Scroom", "index.html", [
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classColormappableMock.html#ab440d6e838c02570f8aa200ca5329e5e",
+"classOperationsCMYK4.html#aff65ad0278d2196c8827ec739a6e5429",
+"classScroom_1_1Detail_1_1ThreadPool_1_1FunctionMultiplier.html#a6cca3693ac58c642d4130d785383597d",
+"classScroom_1_1Utils_1_1ProgressInterfaceMultiplexer_1_1Child.html#a802cc625b68db1289407e0e781175a07",
+"classTiffPresentation.html#acb5e72bdaf524ca35f0820d6571ce7f7",
+"classView.html#a3f31859c80462505442d8f0b4268a2e8",
+"functions_vars_k.html",
+"scroomplugin_8hh.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
