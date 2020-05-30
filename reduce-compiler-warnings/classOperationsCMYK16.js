c7088174d947d20c5d5f91206e6789ec676273c8
Documentation Builder <project@mandryn.org>
2020-05-30 08:45:27 +0000

    Automated documentation build for branch reduce-compiler-warnings, changeset bd7444cd21bd3ffe20d766caec1b70c5510c93de.

---
+++ reduce-compiler-warnings/classOperationsCMYK16.js
@@ -0,0 +1,9 @@
+var classOperationsCMYK16 =
+[
+    [ "OperationsCMYK16", "classOperationsCMYK16.html#ab0facecd39b0ed9dfcebbca8a6da7811", null ],
+    [ "~OperationsCMYK16", "classOperationsCMYK16.html#a0c6de59166f962e3aa61c62b952709bd", null ],
+    [ "cache", "classOperationsCMYK16.html#add30970b0c17832b7c827ee7a0332ec5", null ],
+    [ "create", "classOperationsCMYK16.html#ada566d2e02280890405545d1b58677f9", null ],
+    [ "getBpp", "classOperationsCMYK16.html#a12e5741f1fe99cf6d360727febc2e1a5", null ],
+    [ "reduce", "classOperationsCMYK16.html#a8e55f0135be1d3d96c125b449fd0e6d5", null ]
+];
