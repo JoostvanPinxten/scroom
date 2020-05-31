1886ca4286d3fac1c1e4bfe57400dd5f9579ccdd
Documentation Builder <project@mandryn.org>
2020-05-31 18:33:24 +0000

    Automated documentation build for branch cmyk-with-sampleiterators, changeset cd034434b72bd93a1751457923bd40f1a4af24d3.

---
+++ cmyk-with-sampleiterators/classOperationsCMYK.js
@@ -0,0 +1,10 @@
+var classOperationsCMYK =
+[
+    [ "OperationsCMYK", "classOperationsCMYK.html#a43c9c4b05511a0067cd4887ff80d48eb", null ],
+    [ "~OperationsCMYK", "classOperationsCMYK.html#adff68de42e84d115132b5ceb0aed6cc9", null ],
+    [ "cache", "classOperationsCMYK.html#a6088c4e2138bc1c0f6325749bb0c4933", null ],
+    [ "create", "classOperationsCMYK.html#ab788eb469def1dc2fb16fa489113ba60", null ],
+    [ "getBpp", "classOperationsCMYK.html#a9720849e3ba77f29bc862a937aba7175", null ],
+    [ "reduce", "classOperationsCMYK.html#a23f5535415e1b7f2ba01daf88b86b5e3", null ],
+    [ "bps", "classOperationsCMYK.html#a522040dbc6e26d5db6be23a0a8f4480f", null ]
+];
---
+++ cmyk-with-sampleiterators/navtreedata.js
@@ -0,0 +1,56 @@
+var NAVTREE =
+[
+  [ "Scroom", "index.html", [
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classColormappableMock.html#ab440d6e838c02570f8aa200ca5329e5e",
+"classPluginInformationInterface.html#aae9249c0ff8a018ca4306f8e898e2e10",
+"classScroom_1_1Detail_1_1ThreadPool_1_1QueueLock.html#a168151543f6766a8ce874377f3d4408e",
+"classScroom_1_1Utils_1_1ProgressStateInterface.html#af17b9367dd613c97f7a56479638aff55",
+"classTiffPresentationWrapper.html#a3fc7e574ed3889e979e544a49f83664a",
+"classView.html#a757f82b654de6a716386fbd4063f95f9",
+"functions_z.html",
+"structMeasurement.html#a21305fec9977e086f392c9622f81aae6"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
